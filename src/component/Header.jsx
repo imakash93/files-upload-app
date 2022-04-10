2b322358756825dc8ee1de0d5fdacf1bb75186a4
Akash Goswami <devil.dudleypoizen@gmail.com>
2022-04-11 04:50:50 +0530

    changes in nav

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,15 +5,8 @@ function Header() {
   return (
     <Container fluid>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
+        <Container fluid>
           <Navbar.Brand href="#home">File Upload App</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-            </Nav>
-            <Nav></Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </Container>
